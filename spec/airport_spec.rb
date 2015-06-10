e4b9ae456eca65d73990c7a198f91dbc3c623bff
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-06-10 15:05:12 +0100

    written tests

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,1 +1,21 @@
 require 'airport'
+
+describe Airport do
+  xit 'has a capacity' do
+  end
+
+  xit 'has a control tower that knows if the weather is sunny or stormy' do
+  end
+
+  xit 'allow a plane to land if the weather is sunny' do
+  end
+
+  xit 'does not allow a plane if the weather is stormy' do
+  end
+
+  xit 'allow a plane to land if it is not full' do
+  end
+
+  xit 'does not allow a plane to land if it is full' do
+  end
+end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,1 +1,18 @@
 require 'plane'
+
+describe Plane do
+  xit 'has a flying status when created' do
+  end
+
+  xit 'can land' do
+  end
+
+  xit 'can fly again when landed' do
+  end
+
+  xit 'cannot land a second time if it has already landed' do
+  end
+
+  xit 'cannot fly a second time if it is already flying' do
+  end
+end
